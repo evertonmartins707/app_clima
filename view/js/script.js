dd4ffe6233569fa0dffeff28a6eb57ab8648fdbb
Everton Martins <evertonmartins707@gmail.com>
2023-11-14 21:46:31 -0300

    Events

--- view/js/script.js
+++ view/js/script.js
@@ -1,18 +1,25 @@
 import * as weatherApi from "./api_weather.js";
 
-// Elementos HTML
+// Elememts HTML
 const titleCity = document.querySelector("#city_name")
 const citySearch = document.querySelector("#search_Field");
 const btnSearch = document.querySelector("#btn_search");
 const objectUser = document.querySelector(".object_user");
 let city = undefined;
 
+// Events
+document.addEventListener("keydown", keyPressed)
+btnSearch.addEventListener("click", requestApi)
 
-btnSearch.addEventListener("click", async () => {
+// Funtions
+function keyPressed(){
+    var tecla = event.keyCode;
+    if(!String.fromCharCode(tecla) == " "){requestApi()}
+}
+
+async function requestApi() {
     city = citySearch.value
     const objectData = await weatherApi.resultApi(city);
 
     titleCity.innerHTML = objectData["city"]
-    objectUser.textContent = objectData["temp"]
-    console.log(objectData)
-});
+};
