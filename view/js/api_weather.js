09e92445718ea71564853303d727dc67cdc2e70f
Everton Martins <evertonmartins707@gmail.com>
2023-11-14 22:59:24 -0300

    comentário e estilizações

--- view/js/api_weather.js
+++ view/js/api_weather.js
@@ -1,23 +1,26 @@
-// Usando a API: HG Brasil Weather
-// https://hgbrasil.com/
+// API: HG Brasil Weather
+// Site da API: https://hgbrasil.com/
+// Documentação: https://console.hgbrasil.com/documentation/weather
 
 // Objeto da API
-
 const api = {
-    baseApi: 'https://api.hgbrasil.com/weather',
-    key: '40df055f',
-    returnFormat: 'json-cors',
+    baseApi: 'https://api.hgbrasil.com/weather', // EndPoint base
+    key: '40df055f', // Chave de acesso
+    returnFormat: 'json-cors' // Formato de resposta
 };
+// Palavras-chave de informações de resposta da API
 const valuesRequireds = ["temp", "date", "time", "city", "max", "min", "description", "currently", "humidity", "forecast" ]
 
 // -----------------------
 
 export const resultApi = (value_city) => {
-    const result = fetch(`${api.baseApi}?format=${api.returnFormat}&array_limit=7&fields=only_results,${valuesRequireds}&key=${api.key}&city_name=${value_city}`)
-    .then((res) => res.json())
-    .then((data) => {
-        return data;
-    });
-    return result;
-
+    try {
+        const result = fetch(`${api.baseApi}?format=${api.returnFormat}&array_limit=7&fields=only_results,${valuesRequireds}&key=${api.key}&city_name=${value_city}`) // Link de chamada para API seguindo todos os parâmetros
+        .then((res) => res.json()) // Retorna o resultadcurrent previsiono em um arquivo JSON
+        .then((data) => { // Obtém os dados do arquivo JSON
+            return data;
+        });
+        return result;
+    }
+    catch {alert("Não foi possível localizar a cidade")};
 }
