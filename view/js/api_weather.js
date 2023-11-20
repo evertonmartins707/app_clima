0b7a4cca257e7b9e540d9204f82f95b7184aeb2a
Everton Martins <evertonmartins707@gmail.com>
2023-11-20 16:44:59 -0300

    tratametno de erros

--- view/js/api_weather.js
+++ view/js/api_weather.js
@@ -14,30 +14,21 @@ const valuesRequireds = ["temp", "date", "time", "city", "max", "min", "descript
 // -----------------------
 
 export const resultApiCity = (value_city) => {
-    try {
-        const result = fetch(`${api.baseApi}?format=${api.returnFormat}&array_limit=7&fields=only_results,${valuesRequireds}&key=${api.key}&city_name=${value_city}`, {
-            "method": "POST",
-            "headers": {
-                "Content-Type": "application/json"
-            }
-            }) // Link de chamada para API seguindo todos os parâmetros
-        .then((res) => res.json()) // Retorna o resultadcurrent previsiono em um arquivo JSON
-        .then((data) => { // Obtém os dados do arquivo JSON
-            return data;
-        });
-        return result;
-    }
-    catch {alert("Não foi possível localizar a cidade")};
+    const result = fetch(`${api.baseApi}?format=${api.returnFormat}&array_limit=7&fields=only_results,${valuesRequireds}&key=${api.key}&city_name=${value_city}`) // Link de chamada para API seguindo todos os parâmetros
+    .then((res) => res.json()) // Retorna o resultadcurrent previsiono em um arquivo JSON
+    .then((data) => { // Obtém os dados do arquivo JSON
+        return data;
+    })
+    .catch(err => alert("Não foi possível localizar a cidade", err));
+    return result;
 }
 
 export const resultApiWoeid = () => {
-    try {
-        const result = fetch(`${api.baseApi}?format=${api.returnFormat}&woeid=455819`) // Link de chamada para API seguindo todos os parâmetros
-        .then((res) => res.json()) // Retorna o resultadcurrent previsiono em um arquivo JSON
-        .then((data) => { // Obtém os dados do arquivo JSON
-            return data;
-        });
-        return result;
-    }
-    catch {alert("Não foi possível localizar a cidade")};
+    const result = fetch(`${api.baseApi}?format=${api.returnFormat}&woeid=455819`) // Link de chamada para API seguindo todos os parâmetros
+    .then((res) => res.json()) // Retorna o resultadcurrent previsiono em um arquivo JSON
+    .then((data) => { // Obtém os dados do arquivo JSON
+        return data;
+    })
+    .catch(err => alert("Não foi possível localizar a cidade", err));
+    return result;
 }
